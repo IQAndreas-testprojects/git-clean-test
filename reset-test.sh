3dc204a07699218d38314c5e9d4a23edfdb6e027
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2012-07-03 00:22:01 +0200

    Added parameter to `reset-test.sh` for specifying which test to run

--- reset-test.sh
+++ reset-test.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-cp -r reset-files/ignore ignore
-cp reset-files/untracked-file ./
+
+if [ $# -lt 1 ]; then
+    echo "Please specify which test number you want to run (see README.md for help)."
+elif [ $1 -lt 1 ]; then
+    echo "Invalid test number"
+elif [ $1 -gt 3 ]; then
+	echo "Invalid test number."
+else
+
+	# Remove old files
+	rm -rf ignore
+	rm -f untracked-file
+	
+	# Add new files
+    cp -r reset-files/test$1/* .
+
+fi
