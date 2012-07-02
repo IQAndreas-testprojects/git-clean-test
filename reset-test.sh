24dcf89cbd03f65592dbfe63da87a7e78d072ff8
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2012-07-03 00:47:57 +0200

    Added `reset` option to `reset-test.sh`
    Reset everything to the beginning by running `./reset-test.sh 0`

--- reset-test.sh
+++ reset-test.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ]; then
     echo "Please specify which test number you want to run (see README.md for help)."
-elif [ $1 -lt 1 ]; then
+elif [ $1 -lt 0 ]; then
     echo "Invalid test number"
 elif [ $1 -gt 3 ]; then
 	echo "Invalid test number."
@@ -12,10 +12,14 @@ else
 	rm -rf ignore
 	rm -f untracked-file
 	
-	# Add new files
-    cp -r reset-files/test$1 ignore
-    cp reset-files/untracked-file .
-    cp reset-files/test$1.gitignore .gitignore
-    
+	# Pass 0 to reset everything
+	if [ $1 -eq 0 ]; then
+		cp reset-files/reset.gitignore .gitignore
+	else
+		# Add new files
+		cp -r reset-files/test$1 ignore
+		cp reset-files/untracked-file .
+		cp reset-files/test$1.gitignore .gitignore
+    fi
 
 fi
